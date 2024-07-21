5337891a633e169e6dbe05bafb4bdc563a35b0a2
aditya90205 <adityasrivastav90205@gmail.com>
2024-07-21 13:35:30 +0530

    Remove some errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    target: 'esnext' // Change to 'esnext' or another modern target
+  }
+});
